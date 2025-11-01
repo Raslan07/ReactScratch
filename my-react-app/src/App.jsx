6c490df2d935c4e1ee898b567fb4393b5c8d6f09
Mostafa Raslan <raslanspeed007@gmail.com>
2025-11-01 10:10:18 +0200

    V8

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,53 +1,78 @@
 import { useState } from "react";
+import "./styles.css";
 
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
 
 export default function App() {
   return (
-    <div className="app">
-    <Form />
-  </div>
-);
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
 }
 
-
-function Form(){
-  const [step , setStep] = useState(1);
-  const [day , setDay] = useState(0);
-
-   const data = new Date("june 21 2027");
-   data.setDate(data.getDate() + day);
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
 
   return (
-    <div className="container">
-      
-      <div className="ranger">
-        <input
-          type="range"
-          value={step}
-          id="range"
-          min={1}
-          max={10}
-          onChange={(e) => setStep(Number(e.target.value))}
-        />
-        <span id="">step : {step}</span>
-      </div>
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
+      ))}
 
-      <div className="puts">
-        <button onClick={() => setDay((e)=> e - step)}>-</button>
-        <input
-          type="text"
-          name="text"
-          id="text"
-          value={day}
-          onChange={(e) => setDay(e.target.value)}
-        />
-        <button onClick={() => setDay((e)=>e + step)}>+</button>
-      </div>
-      <p>
-        <span>{day}</span> days from today is {data.toDateString()}{" "}
-      </p>
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Test 1"
+        num={22}
+        key="test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusuable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
+    </div>
+  );
+}
 
-      
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
